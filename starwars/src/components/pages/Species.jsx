1adadc598c4122ffc5cfea2ce772d21ccf7359c2
Danielecarn <dannygoncalves41@gmail.com>
2022-08-16 01:42:03 -0300

    Componente de species

--- starwars/src/components/pages/Species.jsx
+++ starwars/src/components/pages/Species.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import Card from "../Card";
+import Loading from "../layout/Loading";
+import { getUrlId } from "../utils/getUrlId";
 
 const Species = () => {
+  const [species, setSpecies] = useState(null);
+  
+  useEffect(() => {
+    api.get("/species/")
+       .then((response) => {
+         console.log(response);
+         setSpecies(response.data.results)
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+      });
+  }, []);
+
   return (
-    <div>Species</div>
+    <>
+      <div>Species</div>
+      {Array.isArray(species) ? species.map(specie => (
+          <Card key={specie.name} imgURL={`https://starwars-visualguide.com/assets/img/species/${getUrlId(
+            specie.url,)}.jpg`} title={specie.name}/>
+        ))
+        : (<Loading/>)}
+    </>
   )
 }
 
