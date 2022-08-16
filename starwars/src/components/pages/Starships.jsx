ede323c5a1e8086d623d66c737580168347ed772
Danielecarn <dannygoncalves41@gmail.com>
2022-08-16 01:46:16 -0300

    Componente para exibir as starships

--- starwars/src/components/pages/Starships.jsx
+++ starwars/src/components/pages/Starships.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import Card from "../Card";
+import Loading from "../layout/Loading";
+import { getUrlId } from "../utils/getUrlId";
 
 const Starships = () => {
+  const [starships, setStarships] = useState(null);
+  
+  useEffect(() => {
+    api.get("/starships/")
+       .then((response) => {
+         console.log(response);
+         setStarships(response.data.results)
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+      });
+  }, []);
+
   return (
-    <div>Starships</div>
+    <>
+      <div>Starships</div>
+      {Array.isArray(starships) ? starships.map(starship => (
+          <Card key={starship.name} imgURL={`https://starwars-visualguide.com/assets/img/starships/${getUrlId(
+            starship.url,)}.jpg`} title={starship.name}/>
+        ))
+        : (<Loading/>)}
+    </>
   )
 }
 
