37e6872ed77c91d389e828b21527f65f2a7293dd
Danielecarn <dannygoncalves41@gmail.com>
2022-08-17 22:31:37 -0300

    Detalhes das starships

--- starwars/src/components/pages/Starship.jsx
+++ starwars/src/components/pages/Starship.jsx
@@ -5,6 +5,8 @@ import Card from "../Card";
 import Loading from "../layout/Loading";
 import { getUrlId } from "../utils/getUrlId";
 
+import{BiNavigation} from "react-icons/bi";
+
 import './Film.css';
 
 const Starship = () => {
@@ -42,6 +44,60 @@ const Starship = () => {
             object= {starship}
             showLink = {false}
           />
+          <div className="info">
+            <h3>
+                <BiNavigation/> Modelo:
+            </h3>
+            <p>{starship.model}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Fabricante:
+            </h3>
+            <p>{starship.manufacturer}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Custo em créditos:
+            </h3>
+            <p>{starship.cost_in_credits}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Comprimento:
+            </h3>
+            <p>{starship.length}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Velocidade Máxima na Atmosfera:
+            </h3>
+            <p>{starship.max_atmosphering_speed}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Passageiros:
+            </h3>
+            <p>{starship.passengers}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Capacidade de Carga:
+            </h3>
+            <p>{starship.cargo_capacity}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Pilotos:
+            </h3>
+            <p>{starship.pilots}</p>
+          </div>
+          <div className="info">
+            <h3>
+                <BiNavigation/> Filmes:
+            </h3>
+            <p>{starship.films}</p>
+          </div>
         </>
       : (<Loading/>)}
 
