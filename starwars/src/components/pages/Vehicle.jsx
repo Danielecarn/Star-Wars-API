fcc205f5d805912b13b253cbe351c1a68476baa4
Danielecarn <dannygoncalves41@gmail.com>
2022-08-17 23:10:26 -0300

    Vehicle request

--- starwars/src/components/pages/Vehicle.jsx
+++ starwars/src/components/pages/Vehicle.jsx
@@ -1,5 +1,26 @@
 
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+import './Film.css';
+
 const Vehicle = () => {
+
+  const id = useParams()
+  
+  const [film, setFilm] = useState(null);
+  
+  useEffect(() => {
+    api.get(`/vehicles/${id.id}`)
+       .then((response) => {
+         console.log(response);
+         setFilm(response.data)
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+      });
+  }, []);
+
   return (
     <div>Vehicle</div>
   )
