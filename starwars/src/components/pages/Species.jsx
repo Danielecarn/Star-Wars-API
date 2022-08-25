da366432e1923463533fafdda162ce54a70e23b6
Danielecarn <dannygoncalves41@gmail.com>
2022-08-25 00:31:05 -0300

    Species pagination

--- starwars/src/components/pages/Species.jsx
+++ starwars/src/components/pages/Species.jsx
@@ -4,29 +4,42 @@ import api from "../../services/api";
 
 import Card from "../Card";
 import Loading from "../layout/Loading";
+import Pagination from "../layout/Pagination";
 
 import { getUrlId } from "../utils/getUrlId";
 
 import './CardsGrid.css';
 
 const Species = () => {
-  const [species, setSpecies] = useState(null);
+  const [species, setSpecies] = useState();
+  const [qtdSpecies, setQtdPeople] = useState(Number);
+  const [currentPage, setCurrentPage] = useState(1);
   
   useEffect(() => {
-    api.get("/species/")
+    api.get(`/species/${currentPage != 1 ? '?page='+currentPage : ""}`)
        .then((response) => {
-         console.log(response);
          setSpecies(response.data.results)
+         setQtdPeople(response.data.count)
       })
       .catch((err) => {
         console.error("ops! ocorreu um erro : " + err);
       });
-  }, []);
+  }, [currentPage]);
+
+  const pagesNumber = Math.ceil(qtdSpecies/10);
 
   return (
     <div className="container">
       <div className="title">
         <h3>Espécies</h3>
+      </div>
+      <div className="paginacao">
+        <Pagination
+          isFirst={currentPage ===1}
+          isLast={currentPage === pagesNumber}
+          navigateBack={() => setCurrentPage(currentPage-1)}
+          navigateNext={() => setCurrentPage(currentPage+1)}
+        />
       </div>
       <div className="cards-container">
         {Array.isArray(species) ? species.map(specie => (
